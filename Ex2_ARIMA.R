2133398f73fc96b3daa4dc18dbb08f889cdee6a7
Tom <thomashollis1@gmail.com>
2017-10-16 19:06:36 +0100

    Add files via upload

---
+++ Ex2_ARIMA.R
@@ -0,0 +1,30 @@
+data <- read.csv("R/BTC400h.csv")
+
+data_train <- data[1:282,2]
+data_test <- data[283:376,2]
+
+btc_ar <- arima(data_train, order = c(3, 2, 1), method = "CSS")
+btc_pred <- predict(object = btc_ar, n.ahead = 94)
+
+x1 <- seq(283,376,1)
+plot(data_train, ylim=range(data[,2]), xlim=range(1,376), xlab="Hours Elapsed", ylab="BTC Price (USD)", type = "l", col = "black")
+lines(x = x1, data_test, ylim=range(data[,2]), xlim=range(1,376), axes = FALSE, xlab = "", ylab = "", type = "l", col = "black")
+lines(x = x1, btc_pred$pred, ylim=range(data[,2]), xlim=range(1,376), axes = FALSE, xlab = "", ylab = "", type = "l", col = "red")
+
+btc_ar <- arima(data_train, order = c(1,2,3), method = "CSS")
+btc_pred <- predict(object = btc_ar, n.ahead = 94)
+lines(x = x1, btc_pred$pred, ylim=range(data[,2]), xlim=range(1,376), axes = FALSE, xlab = "", ylab = "", type = "l", col = "green")
+
+btc_ar <- arima(data_train, order = c(3,4,1), method = "CSS")
+btc_pred <- predict(object = btc_ar, n.ahead = 94)
+lines(x = x1, btc_pred$pred, ylim=range(data[,2]), xlim=range(1,376), axes = FALSE, xlab = "", ylab = "", type = "l", col = "blue")
+
+btc_ar <- arima(data_train, order = c(1,2,5), method = "CSS")
+btc_pred <- predict(object = btc_ar, n.ahead = 94)
+lines(x = x1, btc_pred$pred, ylim=range(data[,2]), xlim=range(1,376), axes = FALSE, xlab = "", ylab = "", type = "l", col = "brown")
+
+btc_ar <- arima(data_train, order = c(1,3,3), method = "CSS")
+btc_pred <- predict(object = btc_ar, n.ahead = 94)
+lines(x = x1, btc_pred$pred, ylim=range(data[,2]), xlim=range(1,376), axes = FALSE, xlab = "", ylab = "", type = "l", col = "pink")
+
+legend("topleft", legend=c("Coefficients: 3, 2, 1", "Coefficients: 1, 2, 3", "Coefficients: 3, 4, 1", "Coefficients: 1, 2, 5", "Coefficients: 1, 3, 3"), col=c("red", "green", "blue", "brown", "pink"), bty = "n", cex = 0.9, pch = 16, text.col = "black")
